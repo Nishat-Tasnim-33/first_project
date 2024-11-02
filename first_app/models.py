1ee0f4ba977ded632de3765131395516d6059688
Nishat <ntasnimrk@gmail.com>
2024-11-02 11:44:54 +0600

    1

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.Charfield(max_length=200)
+    author = models.Charfield(max_length=200)
+    published_date = models.Datefield()
+
+    def __str__(self):
+        return self.title
---
+++ first_app/views/fbv_views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from ..models import Book
+def book_list(request):
+
+    books = Book.objects.all() # retrive all books db
+    context = {
+        'books':books,
+        'massage': 'This is FBVs'
+    }
+    return render(request, 'books/book_list.html',context)
